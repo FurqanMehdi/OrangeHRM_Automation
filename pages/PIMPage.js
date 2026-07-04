a67585a54166c65d98435cbbbe6d319c56e0224c
FurqanMehdi <furqanmehdi831@gmail.com>
2026-07-04 14:49:33 +0500

    Added PIM Edit Employee automation

--- pages/PIMPage.js
+++ pages/PIMPage.js
@@ -5,7 +5,7 @@ class PIMPage {
     constructor(page) {
         this.page = page;
 
-        // Add Employee
+        // ---------- Add Employee ----------
         this.addEmployee = page.getByRole('link', { name: 'Add Employee' });
 
         this.firstName = page.locator('input[name="firstName"]');
@@ -14,10 +14,14 @@ class PIMPage {
 
         this.saveButton = page.getByRole('button', { name: 'Save' });
 
-        this.personalDetails = page.getByRole('heading', { name: 'Personal Details' });
+        this.personalDetails = page.getByRole('heading', {
+            name: 'Personal Details'
+        });
 
-        // Employee List (NEW)
-        this.employeeList = page.getByRole('link', { name: 'Employee List' });
+        // ---------- Employee List ----------
+        this.employeeList = page.getByRole('link', {
+            name: 'Employee List'
+        });
 
         this.employeeName = page.getByRole('textbox', {
             name: 'Type for hints...'
@@ -27,23 +31,37 @@ class PIMPage {
             name: 'Search'
         });
 
-       this.noRecordsFound = page.locator('span').filter({
-    hasText: 'No Records Found'
-});
+        this.noRecordsFound = page.locator('span').filter({
+            hasText: 'No Records Found'
+        });
+
+        // ---------- Edit Employee ----------
+        this.editButton = page.getByRole('button').filter({
+            hasText: /^$/
+        }).nth(5);
+
+        this.middleNameInput = page.getByRole('textbox', {
+            name: 'Middle Name'
+        });
+
+        this.savePersonalDetails = page
+            .locator('form')
+            .filter({ hasText: 'Employee Full NameEmployee' })
+            .getByRole('button', { name: 'Save' });
+
+        this.successToast = page.getByText('Successfully Updated');
     }
 
-    // ---------------- Add Employee ----------------
+    // ---------- Add Employee ----------
 
     async openAddEmployee() {
         await this.addEmployee.click();
     }
 
     async addEmployeeData(employee) {
-
         await this.firstName.fill(employee.firstName);
         await this.middleName.fill(employee.middleName);
         await this.lastName.fill(employee.lastName);
-
         await this.saveButton.click();
     }
 
@@ -51,7 +69,7 @@ class PIMPage {
         await expect(this.personalDetails).toBeVisible();
     }
 
-    // ---------------- Search Employee ----------------
+    // ---------- Employee List ----------
 
     async openEmployeeList() {
         await this.employeeList.click();
@@ -66,6 +84,23 @@ class PIMPage {
         await expect(this.noRecordsFound).toBeVisible();
     }
 
+    // ---------- Edit Employee ----------
+
+    async openEmployee() {
+        await this.editButton.click();
+    }
+
+    async updateMiddleName(middleName) {
+        await this.middleNameInput.fill(middleName);
+    }
+
+    async saveEmployee() {
+        await this.savePersonalDetails.click();
+    }
+
+    async verifyEmployeeUpdated() {
+        await expect(this.successToast).toBeVisible();
+    }
 }
 
 module.exports = PIMPage;
