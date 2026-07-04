8841c47f07e71dd5d20ea4fa780bf6dee60a8c23
FurqanMehdi <furqanmehdi831@gmail.com>
2026-07-04 14:54:00 +0500

    Completed PIM CRUD automation

--- pages/PIMPage.js
+++ pages/PIMPage.js
@@ -1,106 +1,139 @@
-const { expect } = require('@playwright/test');
+const { expect } = require("@playwright/test");
 
 class PIMPage {
-
-    constructor(page) {
-        this.page = page;
-
-        // ---------- Add Employee ----------
-        this.addEmployee = page.getByRole('link', { name: 'Add Employee' });
-
-        this.firstName = page.locator('input[name="firstName"]');
-        this.middleName = page.locator('input[name="middleName"]');
-        this.lastName = page.locator('input[name="lastName"]');
-
-        this.saveButton = page.getByRole('button', { name: 'Save' });
-
-        this.personalDetails = page.getByRole('heading', {
-            name: 'Personal Details'
-        });
-
-        // ---------- Employee List ----------
-        this.employeeList = page.getByRole('link', {
-            name: 'Employee List'
-        });
-
-        this.employeeName = page.getByRole('textbox', {
-            name: 'Type for hints...'
-        }).first();
-
-        this.searchButton = page.getByRole('button', {
-            name: 'Search'
-        });
-
-        this.noRecordsFound = page.locator('span').filter({
-            hasText: 'No Records Found'
-        });
-
-        // ---------- Edit Employee ----------
-        this.editButton = page.getByRole('button').filter({
-            hasText: /^$/
-        }).nth(5);
-
-        this.middleNameInput = page.getByRole('textbox', {
-            name: 'Middle Name'
-        });
-
-        this.savePersonalDetails = page
-            .locator('form')
-            .filter({ hasText: 'Employee Full NameEmployee' })
-            .getByRole('button', { name: 'Save' });
-
-        this.successToast = page.getByText('Successfully Updated');
-    }
+  constructor(page) {
+    this.page = page;
 
     // ---------- Add Employee ----------
+    this.addEmployee = page.getByRole("link", { name: "Add Employee" });
 
-    async openAddEmployee() {
-        await this.addEmployee.click();
-    }
+    this.firstName = page.locator('input[name="firstName"]');
+    this.middleName = page.locator('input[name="middleName"]');
+    this.lastName = page.locator('input[name="lastName"]');
 
-    async addEmployeeData(employee) {
-        await this.firstName.fill(employee.firstName);
-        await this.middleName.fill(employee.middleName);
-        await this.lastName.fill(employee.lastName);
-        await this.saveButton.click();
-    }
+    this.saveButton = page.getByRole("button", { name: "Save" });
 
-    async verifyEmployeeAdded() {
-        await expect(this.personalDetails).toBeVisible();
-    }
+    this.personalDetails = page.getByRole("heading", {
+      name: "Personal Details",
+    });
 
     // ---------- Employee List ----------
+    this.employeeList = page.getByRole("link", {
+      name: "Employee List",
+    });
 
-    async openEmployeeList() {
-        await this.employeeList.click();
-    }
+    this.employeeName = page
+      .getByRole("textbox", {
+        name: "Type for hints...",
+      })
+      .first();
 
-    async searchEmployee(employeeName) {
-        await this.employeeName.fill(employeeName);
-        await this.searchButton.click();
-    }
+    this.searchButton = page.getByRole("button", {
+      name: "Search",
+    });
 
-    async verifyNoRecordsFound() {
-        await expect(this.noRecordsFound).toBeVisible();
-    }
+    this.noRecordsFound = page.locator("span").filter({
+      hasText: "No Records Found",
+    });
 
     // ---------- Edit Employee ----------
+    this.editButton = page
+      .getByRole("button")
+      .filter({
+        hasText: /^$/,
+      })
+      .nth(5);
 
-    async openEmployee() {
-        await this.editButton.click();
-    }
+    this.middleNameInput = page.getByRole("textbox", {
+      name: "Middle Name",
+    });
 
-    async updateMiddleName(middleName) {
-        await this.middleNameInput.fill(middleName);
-    }
+    this.savePersonalDetails = page
+      .locator("form")
+      .filter({ hasText: "Employee Full NameEmployee" })
+      .getByRole("button", { name: "Save" });
 
-    async saveEmployee() {
-        await this.savePersonalDetails.click();
-    }
+    this.successToast = page.getByText("Successfully Updated");
 
-    async verifyEmployeeUpdated() {
-        await expect(this.successToast).toBeVisible();
-    }
+    // ---------- Delete Employee ----------
+
+    this.deleteButton = page
+      .getByRole("button")
+      .filter({
+        hasText: /^$/,
+      })
+      .nth(4);
+
+    this.confirmDeleteButton = page.getByRole("button", {
+      name: " Yes, Delete",
+    });
+
+    this.successDeleteToast = page.getByText("Successfully Deleted");
+  }
+
+  // ---------- Add Employee ----------
+
+  async openAddEmployee() {
+    await this.addEmployee.click();
+  }
+
+  async addEmployeeData(employee) {
+    await this.firstName.fill(employee.firstName);
+    await this.middleName.fill(employee.middleName);
+    await this.lastName.fill(employee.lastName);
+    await this.saveButton.click();
+  }
+
+  async verifyEmployeeAdded() {
+    await expect(this.personalDetails).toBeVisible();
+  }
+
+  // ---------- Employee List ----------
+
+  async openEmployeeList() {
+    await this.employeeList.click();
+  }
+
+  async searchEmployee(employeeName) {
+    await this.employeeName.fill(employeeName);
+    await this.searchButton.click();
+  }
+
+  async verifyNoRecordsFound() {
+    await expect(this.noRecordsFound).toBeVisible();
+  }
+
+  // ---------- Edit Employee ----------
+
+  async openEmployee() {
+    await this.editButton.click();
+  }
+
+  async updateMiddleName(middleName) {
+    await this.middleNameInput.fill(middleName);
+  }
+
+  async saveEmployee() {
+    await this.savePersonalDetails.click();
+  }
+
+  async verifyEmployeeUpdated() {
+    await expect(this.successToast).toBeVisible();
+  }
+
+  // ---------- Delete Employee ----------
+
+  async deleteEmployee() {
+    await this.deleteButton.click();
+  }
+
+  async confirmDelete() {
+    await this.confirmDeleteButton.click();
+  }
+
+  async verifyEmployeeDeleted() {
+    await expect(this.successDeleteToast).toBeVisible();
+  }
 }
 
 module.exports = PIMPage;
---
+++ tests/pims/deleteEmployee.spec.js
@@ -0,0 +1,49 @@
+const { test } = require('@playwright/test');
+
+const LoginPage = require('../../pages/LoginPage');
+const Sidebar = require('../../components/Sidebar');
+const PIMPage = require('../../pages/PIMPage');
+
+const loginData = require('../../test-data/login.json');
+const employeeData = require('../../test-data/employee.json');
+
+test.describe('PIM - Delete Employee', () => {
+
+    let loginPage;
+    let sidebar;
+    let pimPage;
+
+    test.beforeEach(async ({ page }) => {
+
+        loginPage = new LoginPage(page);
+        sidebar = new Sidebar(page);
+        pimPage = new PIMPage(page);
+
+        await loginPage.navigate();
+
+        await loginPage.login(
+            loginData.validUser.username,
+            loginData.validUser.password
+        );
+
+        await sidebar.openPIM();
+
+        await pimPage.openEmployeeList();
+
+    });
+
+    test('Delete Employee', async () => {
+
+        await pimPage.searchEmployee(
+            employeeData.editEmployee.searchName
+        );
+
+        await pimPage.deleteEmployee();
+
+        await pimPage.confirmDelete();
+
+        await pimPage.verifyEmployeeDeleted();
+
+    });
+
+});
